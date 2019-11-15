8a417c6ddada10557b1d2a5bc1819e346ddc47f5
Hadziq Fabroyir 哈明飛 <hadziq@gmail.com>
2019-11-15 08:27:19 +0700

    Lighting through fragment shader

--- vertex.glsl
+++ vertex.glsl
@@ -5,26 +5,23 @@ attribute vec3 vColor;
 attribute vec3 vNormal;
 
 varying vec3 fColor;
+varying vec3 fNormal;
+varying vec3 fPosition;
+
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
-
-uniform vec3 diffuseColor;
-uniform vec3 diffusePosition; // Titik sumber cahaya
 uniform mat3 normalMatrix;  // Berperan sebagai modelMatrix-nya vektor normal
-uniform vec3 ambientColor;
 
 void main() {
   gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(vPosition, 1.0);
-  
-  vec3 normal = normalize(normalMatrix * vNormal);
-  // Arah cahaya = lokasi titik verteks - lokasi titik sumber cahaya
-  vec3 diffuseDirection = normalize(diffusePosition - vec3(modelMatrix * vec4(vPosition, 1.0)));
-  // Nilai intensitas cahaya = 
-  //  nilai COS sudut antara arah datang cahaya dengan arah vektor normal =
-  //  dot product dari vektor arah datang cahaya • arah vektor normal
-  float normalDotLight = max(dot(normal, diffuseDirection), 0.0);
-  vec3 diffuse = diffuseColor * vColor * normalDotLight;
-  vec3 ambient = ambientColor * vColor;
-  fColor = diffuse + ambient;
+
+  // Transfer warna ke fragment shader
+  fColor = vColor;
+
+  // Transfer vektor normal (yang telah ditransformasi) ke fragment shader
+  fNormal = normalize(normalMatrix * vNormal);
+
+  // Transfer posisi verteks
+  fPosition = vec3(modelMatrix * vec4(vPosition, 1.0));
 }
